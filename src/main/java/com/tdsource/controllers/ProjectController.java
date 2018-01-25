8ebb9fdacf4028a6ee298e02eb34bfbf833f1873
Ovidiu Popoviciu <devovip13@gmail.com>
2018-01-25 22:23:27 +0000

    Add devtools dependency

--- src/main/java/com/tdsource/controllers/ProjectController.java
+++ src/main/java/com/tdsource/controllers/ProjectController.java
@@ -1,4 +1,14 @@
 package com.tdsource.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProjectController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings dear friend!";
+    }
+
 }
