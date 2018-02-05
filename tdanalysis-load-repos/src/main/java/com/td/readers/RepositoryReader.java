36a77cac2a50af34bd98fe406d551d61861cec79
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-05 00:13:31 +0000

    Add repository reader for in-memory store

---
+++ tdanalysis-load-repos/src/main/java/com/td/readers/RepositoryReader.java
@@ -0,0 +1,32 @@
+package com.td.readers;
+
+import com.td.models.RepositoryModel;
+import com.td.store.InMemoryStore;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.stereotype.Component;
+
+/**
+ * ItemReader implementation that reads the repositories from the in-memory store.
+ */
+@Component
+public class RepositoryReader implements ItemReader<RepositoryModel> {
+
+    private int next;
+    private InMemoryStore store;
+
+    public RepositoryReader(InMemoryStore store){
+        assert store != null;
+        this.store = store;
+    }
+
+    @Override
+    public RepositoryModel read() throws Exception {
+        RepositoryModel repositoryModel = null;
+
+        if(next < store.getRepositoryModels().size()){
+            repositoryModel = store.getRepositoryModels().get(next++);
+        }
+
+        return repositoryModel;
+    }
+}
