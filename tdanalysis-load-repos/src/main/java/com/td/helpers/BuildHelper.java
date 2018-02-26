4a7f578f099866d3dc212dd0758f709b951559ea
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-26 23:54:28 +0000

    Remove skip flags from java code, add to csv file

--- tdanalysis-load-repos/src/main/java/com/td/helpers/BuildHelper.java
+++ tdanalysis-load-repos/src/main/java/com/td/helpers/BuildHelper.java
@@ -1,12 +1,5 @@
 package com.td.helpers;
 
-import com.td.models.BuildStatus;
-import com.td.models.RepositoryModel;
-import io.spring.gradle.dependencymanagement.org.apache.maven.model.Build;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,24 +8,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import com.td.models.BuildStatus;
+import com.td.models.RepositoryModel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 public class BuildHelper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BuildHelper.class);
     private static final String BUILD_FAILURE_MESSAGE = "BUILD FAILURE";
 
-    private static final List<String> PERFORMANCE_FLAGS = new ArrayList<String>() {{
-        add("-T");
-        add("4");
-    }};
-
-    private static final List<String> SKIP_FLAGS = new ArrayList<String>() {{
-        add("-DskipTests");
-        add("-Dcobertura.skip=true");
-        add("-Dcheckstyle.skip=true");
-        add("-Dfindbugs.skip=true");
-        add("-Dpmd.skip=true");
-        add("-Dmaven.javadoc.skip=true");
-    }};
+    private static final List<String> PERFORMANCE_FLAGS = new ArrayList<String>() {
+        {
+            add("-T");
+            add("4");
+        }
+    };
 
     @Value("${java.home.path}")
     private String javaHomePath;
@@ -49,7 +42,6 @@ public class BuildHelper {
         ProcessBuilder builder = new ProcessBuilder();
         List<String> commands = new ArrayList<>(Arrays.asList(repositoryModel.getBuildCommand().split(" ")));
         commands.addAll(PERFORMANCE_FLAGS);
-        commands.addAll(SKIP_FLAGS);
 
         builder.command(commands);
         Map<String, String> envs = builder.environment();
