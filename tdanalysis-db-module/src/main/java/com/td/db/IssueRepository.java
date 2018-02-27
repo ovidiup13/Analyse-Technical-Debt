1cf604342eba7a8ecc4fdf4ec14cfdf2fc7395ce
Ovidiu Popoviciu <devovip13@gmail.com>
2018-02-27 00:58:57 +0000

    Add issue repository component

---
+++ tdanalysis-db-module/src/main/java/com/td/db/IssueRepository.java
@@ -0,0 +1,13 @@
+package com.td.db;
+
+import com.td.models.IssueModel;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssueRepository extends MongoRepository<IssueModel, String> {
+
+    List<IssueModel> findIssueModelsByRepositoryId(String repositoryId);
+
+}
