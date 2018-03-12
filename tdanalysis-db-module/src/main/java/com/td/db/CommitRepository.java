d2fd0481cf82a024647ca59fa0c5840ac8d37fcc
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-12 13:01:08 +0000

    Cache all the things

--- tdanalysis-db-module/src/main/java/com/td/db/CommitRepository.java
+++ tdanalysis-db-module/src/main/java/com/td/db/CommitRepository.java
@@ -2,6 +2,7 @@ package com.td.db;
 
 import com.td.models.CommitModel;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -12,12 +13,16 @@ import java.util.List;
 @Repository
 public interface CommitRepository extends MongoRepository<CommitModel, String> {
 
+    @Cacheable("commit")
     CommitModel findCommitModelByShaAndRepositoryId(String sha, String repositoryId, Sort sort);
 
+    @Cacheable("commits")
     List<CommitModel> findCommitModelsByRepositoryId(String id, Sort sort);
 
+    @Cacheable("issueCommits")
     @Query(value = "{'issueIds': '?0'}")
     List<CommitModel> findCommitModelsByIssueModels(String issueId, Sort sort);
 
+    @Cacheable("authorCommits")
     List<CommitModel> findCommitModelsByRepositoryIdAndAuthor(String id, String author, Sort sort);
 }
--- tdanalysis-db-module/src/main/java/com/td/db/IssueRepository.java
+++ tdanalysis-db-module/src/main/java/com/td/db/IssueRepository.java
@@ -2,6 +2,8 @@ package com.td.db;
 
 import com.td.models.IssueModel;
 import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IssueRepository extends MongoRepository<IssueModel, String> {
 
+    @Cacheable("issues")
     List<IssueModel> findIssueModelsByRepositoryId(String repositoryId, Sort sort);
 
+    @Cacheable("issue")
     IssueModel findIssueModelByIssueKeyAndRepositoryId(String issueKey, String repositoryId);
 }
