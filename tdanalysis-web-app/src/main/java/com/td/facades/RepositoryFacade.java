99b7a2f18c4d1e4fb6283eed537272797d37ae12
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-13 02:40:25 +0000

    Add repository facade for aggregating data processing

---
+++ tdanalysis-web-app/src/main/java/com/td/facades/RepositoryFacade.java
@@ -0,0 +1,188 @@
+package com.td.facades;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.td.db.CommitRepository;
+import com.td.db.IssueRepository;
+import com.td.db.ProjectRepository;
+import com.td.models.CommitModel;
+import com.td.models.IssueModel;
+import com.td.models.RepositoryModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RepositoryFacade {
+
+    private static final int OVER_COMMITS_PER_ISSUE = 0;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @Autowired
+    private CommitRepository commitRepository;
+
+    /***
+     * Returns all repositories from the database.
+     */
+    public List<RepositoryModel> getRepositories() {
+        Sort sort = new Sort(Sort.Direction.ASC, "id");
+        return projectRepository.findAll(sort);
+    }
+
+    /***
+     * Returns a RepositoryModel wrapped in an optional. 
+     */
+    public Optional<RepositoryModel> getRepository(String repositoryId) {
+        RepositoryModel result = projectRepository.findOne(repositoryId);
+        return Optional.of(result);
+    }
+
+    /***
+     * Returns a list of all collaborators of the repository.
+     */
+    public List<String> getCollaborators(String id) {
+        return getAllCommits(id).stream().map(commit -> commit.getAuthor()).distinct().collect(Collectors.toList());
+    }
+
+    /***
+     * Returns all issues within the repository.
+     */
+    public List<IssueModel> getAllIssues(String repositoryId) {
+        Sort sort = new Sort(Sort.Direction.ASC, "issueKey");
+        return issueRepository.findIssueModelsByRepositoryId(repositoryId, sort);
+    }
+
+    /***
+     * Returns an {@link Optional} object with an IssueModel object inside.
+     */
+    public Optional<IssueModel> getIssue(String repositoryId, String issueKey) {
+        IssueModel issue = issueRepository.findIssueModelByIssueKeyAndRepositoryId(issueKey, repositoryId);
+        return Optional.of(issue);
+    }
+
+    /***
+     * Returns all commits in the repository.
+     */
+    public List<CommitModel> getAllCommits(String repositoryId) {
+        Sort sort = new Sort(Sort.Direction.ASC, "timestamp");
+        return commitRepository.findCommitModelsByRepositoryId(repositoryId, sort);
+    }
+
+    /***
+     * Returns a single commit from the repository, given its SHA.
+     */
+    public Optional<CommitModel> getCommit(String repositoryId, String sha) {
+        CommitModel result = commitRepository.findCommitModelByShaAndRepositoryId(sha, repositoryId);
+        return Optional.of(result);
+    }
+
+    /***
+     * Returns all commits that reference a specific issue. Commits are sorted by timestamp.
+     */
+    public List<CommitModel> getAllCommitsByIssue(String repoId, String issueKey) {
+        IssueModel issue = issueRepository.findIssueModelByIssueKeyAndRepositoryId(issueKey, repoId);
+
+        if (issue == null) {
+            return new ArrayList<CommitModel>();
+        }
+
+        Sort sort = new Sort(Sort.Direction.ASC, "timestamp");
+        return commitRepository.findCommitModelsByIssueModels(issue.getIssueId(), sort);
+    }
+
+    /**
+    * Returns an array of maps, with each map containing an issue-commits pair.
+    * Issues are sorted by created date while commits are sorted by timestamp.
+    */
+    public List<Map<String, List<CommitModel>>> getIssuesAndCommitsRaw(String repositoryId) {
+        Sort sortCreated = new Sort(Sort.Direction.ASC, "created");
+        Sort sortTimestamp = new Sort(Sort.Direction.ASC, "timestamp");
+        List<IssueModel> issues = issueRepository.findIssueModelsByRepositoryId(repositoryId, sortCreated);
+
+        List<Map<String, List<CommitModel>>> result = new ArrayList<>(issues.size());
+        issues.forEach(issue -> {
+            Map<String, List<CommitModel>> map = new HashMap<>();
+            List<CommitModel> commits = commitRepository.findCommitModelsByIssueModels(issue.getIssueId(),
+                    sortTimestamp);
+            map.put(issue.getIssueKey(), commits);
+            result.add(map);
+        });
+
+        return result;
+    }
+
+    /***
+     * Similar to the getIssuesAndCommitsRaw() method, but result is filtered by
+     * number of commits per issue and by author. 
+     */
+    public List<Map<String, List<CommitModel>>> getIssuesAndCommitsFiltered(String repositoryId) {
+        List<Map<String, List<CommitModel>>> result = getIssuesAndCommitsRaw(repositoryId);
+
+        // filter by author and number of commits
+        return result.parallelStream().map(this::filterByAuthor).filter(this::filterByCount)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Filters the issue-commit map by the number of commits associated with an issue.
+     */
+    private boolean filterByCount(Map<String, List<CommitModel>> item) {
+        Entry<String, List<CommitModel>> entry = item.entrySet().iterator().next();
+        return entry.getValue().size() > OVER_COMMITS_PER_ISSUE;
+    }
+
+    /**
+     * Filters map of issue-commits to only include revisions made by a single author.
+     */
+    private Map<String, List<CommitModel>> filterByAuthor(Map<String, List<CommitModel>> item) {
+        Entry<String, List<CommitModel>> entry = item.entrySet().iterator().next();
+        String issue = entry.getKey();
+        List<CommitModel> commits = filterByAuthor(entry.getValue());
+        item.replace(issue, commits);
+        return item;
+    }
+
+    /**
+     * Returns a list of commits filtered by a single (supposedly) author.  
+     */
+    private List<CommitModel> filterByAuthor(List<CommitModel> commits) {
+        if (commits.isEmpty()) {
+            return commits;
+        }
+        String author = getMainAuthor(commits);
+        return commits.stream().filter(commit -> commit.getAuthor().equals(author)).collect(Collectors.toList());
+    }
+
+    /***
+     * Retrieves the main author of the list of commits.
+     * The "main author" is the author that has the most revisions.
+     */
+    private String getMainAuthor(List<CommitModel> commits) {
+        Map<String, Integer> authorMap = new HashMap<>();
+        commits.stream().forEach(commit -> {
+            String author = commit.getAuthor();
+            if (authorMap.containsKey(author)) {
+                authorMap.replace(author, authorMap.get(author) + 1);
+            } else {
+                authorMap.put(author, 1);
+            }
+        });
+
+        //TODO: what happens if there is a tie?
+        return Collections.max(authorMap.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+    }
+
+}
