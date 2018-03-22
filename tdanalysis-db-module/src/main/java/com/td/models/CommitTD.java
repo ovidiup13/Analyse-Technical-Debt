83ba8e0c4c48120a2117fd5f8d7b107b2e86a424
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-22 20:04:42 +0000

    Cleaning up a few things

--- tdanalysis-db-module/src/main/java/com/td/models/CommitTD.java
+++ tdanalysis-db-module/src/main/java/com/td/models/CommitTD.java
@@ -50,4 +50,39 @@ public class CommitTD {
         this.location = location;
     }
 
+    public class CodeLocation {
+
+        private String fileName;
+        private int lineNumber;
+
+        /**
+         * @return the file name
+         */
+        public String getFileName() {
+            return fileName;
+        }
+
+        /**
+         * @param fileName the file name to set
+         */
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        /**
+         * @return the line number
+         */
+        public int getLine() {
+            return lineNumber;
+        }
+
+        /**
+         * @param line the line number to set
+         */
+        public void setLine(int line) {
+            this.lineNumber = line;
+        }
+
+    }
+
 }
--- tdanalysis-db-module/src/main/java/com/td/models/TechnicalDebtItem.java
+++ tdanalysis-db-module/src/main/java/com/td/models/TechnicalDebtItem.java
@@ -69,7 +69,7 @@ public class TechnicalDebtItem {
         this.category = category;
     }
 
-    public class CompositeKey implements Serializable {
+    public static class CompositeKey implements Serializable {
         private static final long serialVersionUID = -5455868338014840934L;
 
         private Character categoryInitial;
