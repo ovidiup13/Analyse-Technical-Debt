834351dcd242847f92c2aec0a7ea946a3bcd3ee9
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-25 22:02:20 +0100

    Refactor repo processor

--- tdanalysis-load-repos/src/main/java/com/td/Loader.java
+++ tdanalysis-load-repos/src/main/java/com/td/Loader.java
@@ -3,6 +3,7 @@ package com.td;
 import java.util.List;
 
 import com.td.models.RepositoryModel;
+import com.td.processor.RepositoryProcessor;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,11 +12,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Loader {
+
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Loader.class);
 
         RepositoryReader reader = context.getBean(RepositoryReader.class);
         List<RepositoryModel> repos = reader.readRepositories();
-        repos.forEach(repo -> System.out.println(repo.getName()));
+
+        RepositoryProcessor processor = context.getBean(RepositoryProcessor.class);
+        processor.processRepositories(repos);
     }
 }
--- tdanalysis-load-repos/src/main/java/com/td/processor/RepositoryProcessor.java
+++ tdanalysis-load-repos/src/main/java/com/td/processor/RepositoryProcessor.java
@@ -3,6 +3,8 @@ package com.td.processor;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 
 import com.td.helpers.VersionControlHelper;
 import com.td.models.RepositoryModel;
@@ -15,21 +17,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class RepositoryProcessor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(RepositoryProcessor.class);
     private static final String tempFolder = Paths.get(System.getProperty("user.dir"), "tmp").toString();
 
-    public RepositoryModel process(RepositoryModel item) {
-        LOGGER.info(String.format("Reading repository info %s:%s", item.getAuthor(), item.getName()));
-
-        File repoPath = new File(Paths.get(tempFolder, item.getName()).toString());
-        try (VersionControlHelper ignored = repoPath.exists() ? new VersionControlHelper(repoPath)
-                : new VersionControlHelper(item.getURI(), repoPath)) {
-            item.setProjectFolder(repoPath);
-        } catch (IOException | GitAPIException e) {
-            LOGGER.error(String.format("An error occurred when processing repository %s", item.getURI()), e);
-        }
-
-        return item;
+    /**
+     * Process the repositories in the file as follows:
+     * 1. Clones them to local disk or reads them if they are not present.
+     * 2. Process each commit one by one.
+     */
+    public void processRepositories(List<RepositoryModel> repositories) {
+        repositories.stream().parallel().forEach(repo -> {
+            Optional<VersionControlHelper> optVc = readOrCloneRepository(repo);
+            if (optVc.isPresent()) {
+                VersionControlHelper vch = optVc.get();
+                vch.close();
+            }
+        });
     }
 
+    /**
+     * This method will try to clone a repository to the local disk. If the
+     * repository already exists it will try to open it.
+     * @return an {@link Optional} containing a {@link VersionControlHelper} if
+     * successful or null otherwise.  
+     */
+    Optional<VersionControlHelper> readOrCloneRepository(RepositoryModel repo) {
+        logger.info(String.format("Reading repository info %s:%s", repo.getAuthor(), repo.getName()));
+
+        File repoPath = new File(Paths.get(tempFolder, repo.getName()).toString());
+        VersionControlHelper versionControlHelper = null;
+
+        try {
+            versionControlHelper = repoPath.exists() ? new VersionControlHelper(repoPath)
+                    : new VersionControlHelper(repo.getURI(), repoPath);
+            repo.setProjectFolder(repoPath);
+        } catch (IOException | GitAPIException e) {
+            logger.error(String.format("An error occurred when processing repository %s", repo.getURI()), e);
+        } catch (SecurityException e) {
+            logger.error("Read cccess to the specified folder is restricted", e);
+        }
+
+        return Optional.ofNullable(versionControlHelper);
+    }
 }
