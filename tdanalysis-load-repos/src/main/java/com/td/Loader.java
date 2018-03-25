4b9b94fbd822ea28c05505633c6845e8c3cc3da3
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-25 21:27:58 +0100

    Add repository reader for reading from CSV

--- tdanalysis-load-repos/src/main/java/com/td/Loader.java
+++ tdanalysis-load-repos/src/main/java/com/td/Loader.java
@@ -1,12 +1,21 @@
 package com.td;
 
-import org.springframework.boot.SpringApplication;
+import java.util.List;
+
+import com.td.models.RepositoryModel;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Loader {
     public static void main(String[] args) {
-        SpringApplication.run(Loader.class, args);
+        ApplicationContext context = new AnnotationConfigApplicationContext(Loader.class);
+
+        RepositoryReader reader = context.getBean(RepositoryReader.class);
+        List<RepositoryModel> repos = reader.readRepositories();
+        repos.forEach(repo -> System.out.println(repo.getName()));
     }
 }
